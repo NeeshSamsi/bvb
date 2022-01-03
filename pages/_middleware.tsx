663ed3265252f6f7960d4a02fd56f9c5221f1864
Neesh <avaneeshsamsi@gmail.com>
2022-01-03 20:38:03 +0530

    fix: :bug: Type middleware request

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-const Middleware = (req) => {
+const Middleware = (req: NextRequest) => {
   if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase()) return NextResponse.next();
 
   return NextResponse.redirect(`${req.nextUrl.origin}${req.nextUrl.pathname.toLowerCase()}`);
