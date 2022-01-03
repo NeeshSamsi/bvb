b1e33a243b1147e841bedba2726b18ba5deb9579
Neesh <avaneeshsamsi@gmail.com>
2022-01-03 18:46:41 +0530

    feat: :sparkles: Nav folder structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from "next";
 
+import NavBar from "../components/NavBar";
+
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-gray-800">Hello Next x Tailwind</h1>
-    </div>
+    <>
+      <NavBar />
+      <div>
+        <h1 className="text-3xl text-gray-800">Hello Next x Tailwind</h1>
+      </div>
+    </>
   );
 };
 
