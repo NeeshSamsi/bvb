529dac1f556aab3d1d709ea5d99c6d1f6bd0b76c
Neesh <avaneeshsamsi@gmail.com>
2022-01-07 19:15:49 +0530

    feat: ✨Add redirect from receipt to receipts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/receipt",
+        destination: "/receipts",
+        permanent: true,
+      },
+    ]
+  },
 }
