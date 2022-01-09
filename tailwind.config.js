ccf753407f378f85da97d534ec4e7c09aa24ad4f
Neesh <avaneeshsamsi@gmail.com>
2022-01-09 18:40:05 +0530

    feat: grid config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "1/2": "1fr 2fr",
+        "1/3/1": "1fr 3fr 1fr",
+        "1/4/1/3": "1fr 4fr 1fr 3fr",
+      },
+    },
   },
   plugins: [],
-};
+}
