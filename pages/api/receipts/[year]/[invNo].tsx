d0a0b278709c8f7da4e5f829aec6e3e6454b60bf
Neesh <avaneeshsamsi@gmail.com>
2022-01-11 16:10:18 +0530

    feat: GET single receipt endpoint

---
+++ pages/api/receipts/[year]/[invNo].tsx
@@ -0,0 +1,18 @@
+import { Prisma } from "@prisma/client"
+import type { NextApiRequest, NextApiResponse } from "next"
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "GET")
+    return res.status(405).json({ error: "Method Not Allowed" })
+
+  const { year, invNo } = req.query
+  const receipt = await prisma?.receipt.findMany({
+    where: {
+      invoiceNumber: `${year}/${invNo}`,
+    },
+  })
+
+  res.status(200).json(receipt)
+}
+
+export default handler
