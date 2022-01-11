b525121cc3d5e455e695f89c08b220ebe0e78cd4
Neesh <avaneeshsamsi@gmail.com>
2022-01-11 22:27:12 +0530

    fix: :ambulance: New global prisma client export
    I know why I need this but I don't know enough about `declare, global or namespace` and how production vs hot reload development works. I'm going from one stackoverflow answer to another and finally found this one on github. Hopefully this is the one and if it is hopefully one day I know why

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,51 @@
+// import { PrismaClient } from "@prisma/client"
+
+// declare global {
+//   var prisma: PrismaClient | undefined
+// }
+
+// export const prisma = global.prisma || new PrismaClient({})
+
+// if (process.env.NODE_ENV !== "production") global.prisma = prisma
+
+// import { PrismaClient } from "@prisma/client"
+
+// declare global {
+//   interface Global {
+//     prisma: PrismaClient | undefined
+//   }
+// }
+
+// let prisma: PrismaClient
+
+// if (typeof window === "undefined") {
+//   if (process.env.NODE_ENV === "production") {
+//     prisma = new PrismaClient()
+//   } else {
+//     if (!global.prisma) {
+//       global.prisma = new PrismaClient()
+//     }
+
+//     prisma = global.prisma
+//   }
+// }
+
+// export default prisma
+
 import { PrismaClient } from "@prisma/client"
 
-declare global {
-  var prisma: PrismaClient | undefined
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient()
+} else {
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient
+  }
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient()
+  }
+  prisma = globalWithPrisma.prisma
 }
 
-export const prisma = global.prisma || new PrismaClient({})
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma
+export default prisma
