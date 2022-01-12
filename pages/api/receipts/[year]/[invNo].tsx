07de519dfb6119db0333827885f340257ce9cc5c
Neesh <avaneeshsamsi@gmail.com>
2022-01-12 12:09:13 +0530

    feat: ✨ Import global prisma

--- pages/api/receipts/[year]/[invNo].tsx
+++ pages/api/receipts/[year]/[invNo].tsx
@@ -1,15 +1,20 @@
-import prisma from "../../../../lib/prisma"
 import type { NextApiRequest, NextApiResponse } from "next"
+import type { ReceiptWhereInput } from "../../../../types"
+
+import prisma from "../../../../lib/prisma"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "GET")
     return res.status(405).json({ error: "Method Not Allowed" })
 
   const { year, invNo } = req.query
+
+  const where: ReceiptWhereInput = {
+    invoiceNumber: `${year}/${invNo}`,
+  }
+
   const receipt = await prisma?.receipt.findMany({
-    where: {
-      invoiceNumber: `${year}/${invNo}`,
-    },
+    where,
   })
 
   res.status(200).json(receipt)
